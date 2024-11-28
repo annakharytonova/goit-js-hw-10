b7e116fd87cb5b7404c58e9edf41b68f5acf5ad4
Anna Kharytonova <haritonova1512@gmail.com>
2024-11-29 00:21:26 +0200

    add 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,57 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const promiseState = event.target.elements.state.value;
+  const promiseDelay = parseInt(event.target.elements.delay.value, 10);
+
+  if (isNaN(promiseDelay) || promiseDelay < 0) {
+    iziToast.error({
+      message: 'Please enter a valid positive number for delay.',
+      position: 'topRight',
+      theme: 'dark',
+    });
+    return;
+  }
+
+  promiseSubmit(promiseState, promiseDelay)
+    .then(result => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${result}ms`,
+        position: 'topRight',
+        theme: 'dark',
+        backgroundColor: '#28a745',
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${error}ms`,
+        position: 'topRight',
+        theme: 'dark',
+        backgroundColor: '#FF0000',
+      });
+    });
+}
+
+function promiseSubmit(promiseState, promiseDelay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (promiseState === 'fulfilled') {
+        resolve(promiseDelay);
+      } else {
+        reject(promiseDelay);
+      }
+    }, promiseDelay);
+  });
+}
+
+iziToast.show({
+  title: 'Test',
+  message: 'This is a test notification!',
+  position: 'topRight',
+});
